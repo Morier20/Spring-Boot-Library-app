8fd943145e576684d7e5cf278f26a0352037a1de
Denis Jakovec <denis.jakovec04@gmail.com>
2025-03-17 15:08:14 +0200

    Created about section modal window. Menu accessibility from any point in the UI.

--- UI/script.js
+++ UI/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const aboutButton = document.getElementById('aboutButton'); 
+const modalWindow = document.querySelector('.fixed-overlay'); 
+const closeButton = document.querySelector('.closeButton')
+aboutButton.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    modalWindow.style.display = 'flex';
+});
+closeButton.addEventListener('click', () => {
+    modalWindow.style.display = 'none'; 
+});
+modalWindow.addEventListener('click', (event) => {
+    if (event.target === modalWindow) {
+        modalWindow.style.display = 'none';
+    }
+});
